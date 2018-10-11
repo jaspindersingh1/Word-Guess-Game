170f960ac52a28b86fe7ce3078445078bc4d8fd1
Jaspinder Singh <jaspinders31@gmail.com>
2018-10-11 13:22:08 -0400

    cleaned the syntax of the code, include source links that shows how i was able to create my syntax and the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,48 @@
+// source for the code
+    // https://www.youtube.com/watch?v=XQEfWd1lh4Q - this will be used for the button on the page. When the button is clicked randomly select a word
 
-
-
-// user choices
-
-// var userChoices = ["a","b","c","d","e","f","g","h","i","j","k","l",'m',"n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
-// check which key was pressed
-document.onkeyup = function(event) {
-    // determine which key was pressed.
-    var userGuess = event.key;
+    // how to show a random word on your html page
+    // https://stackoverflow.com/questions/33857365/getting-a-random-word-to-display-on-page-load-with-javascript
+    // i can only use parts of this 
 
     // array with all the cities to guess
-    var cities = ["HongKong", "Singapore", "Bangkok", "London", "Paris", "Macau", "NewYorkCity", "Shenzhen", "KualaLumpur", "Antalya", "Istanbul", "Dubai", "Seoul", "Rome", "Phuket", "Guangzhou", "Mecca", "Pattaya", "Taipei", "Miami", "Prague", "Shanghai", "LasVegas", "Milan", "Barcelona", "Moscow", "Amsterdam", "Vienna", "Venice", "LosAngeles", "Lima", "Tokyo", "Johannesburg", "Beijing", "Sofia", "Orlando", "Berlin", "Budapest", "HoChiMinhCity", "Florence", "Madrid", "Warsaw", "Doha", "Nairobi", "Delhi", "Mumbai", "Chennai", "MexicoCity", "Dublin", "SanFrancisco", "Hangzhou", "Denpasar", "SaintPetersburg", "Mugla", "Brussels", "Burgas", "Munich", "Zhuhai", "Sydney", "Edirne", "Toronto", "Lisbon", "Cancun", "Buenos Aires", "Cairo", "PuntaCana", "Suzhou", "Djerba", "Agra", "Krakow", "Bucharest", "SiemReap", "Jaipur", "Honolulu", "Manama", "Dammam", "Hanoi", "AndorraLaVella", "Nice", "Zurich"];
+        
+        var cities = ["hongkong", "singapore", "bangkok", "london", "paris", "macau", "newyorkcity", "shenzhen",  "dubai", "seoul", "rome", "phuket", "guangzhou", "newdehli", "goa", "miami", "prague", "shanghai", "lasvegas", "milan", "barcelona", "moscow"];
 
-    // randomly choose the cities above for the user to guess
-    var guessWord = cities[Math.floor(Math.random() * cities.length)];
+    // choose a random word from above array
 
-    // this will select a word and show it on the html
-    // var wordSelected = document.getElementById("game");
-    // wordSelected.innerHTML = guessWord;
+        var word = cities[Math.floor(Math.random() * cities.length)];
 
-    // answer area
-    var answerField = [];
-    
-    // for loop to select how many underscore depending on the word
-    for (var i = 0; i < guessWord.length; i++) {
-        answerField[i] = "_";
-    }
-    
-    // again this will show the empty space above
-    // var wordSelected = document.getElementById("game");
-    //     wordSelected.innerHTML = answerField;
+    // create the answer array
 
-    // crate var to keep track of letter that remain to be guessed
-    var remainingLetters = guessWord.length;
+        var answerArray = [];
+        for (var i = 0; i < cities.length; i++) {
+            answerArray[i] = "_";
+        }
 
-    // game loop, show progress to the user, get a guess from player
-    // the remaining letter is greater than zero so the user can keep guessing letters
-    if (remainingLetters > 0) {
-        var playerProgress = document.getElementById("game");
-        // join property will get rid of the comma that was coming up
-        playerProgress.innerHTML = answerField.join(" ");
+        var remainingLetters = word.length;
 
-        var playerGuess = document.userGuess;
-        // this should display the letter on the screen.    
-        playerGuess.innerHTML = answerField;
-       
-    }
+    // game loop
 
-}   
+        while (remainingLetters > 0) {
 
-// created a p tag in the html file to display what choice was made
-// create a var that will hold reference to the html of choice made
-// var userChoiceText = document.getElementById("userChoice-text");
+            // alert(answerArray.join(" "));
+            document.getElementById("game")
 
+            // get a guess from the player
+            var guess = prompt("guess a letter or click cancel to stop");
 
+            if (guess === null) {
+                break;
+            } else if (guess.length !== 1) {
+                alert("enter a letter");
+            } else {
+                for (var j = 0; j < cities.length; j++){
+                    if (word[j] === guess) {
+                        answerArray[j] = guess;
+                        remainingLetters--;
+                    }
+                }
+            }
 
-
+        }
